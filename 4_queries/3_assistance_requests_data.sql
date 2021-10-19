fc91a0c3c653456b1b381c23df2bada1072186ad
dav-123 <david.he07@yahoo.ca>
2021-10-19 16:53:02 -0400

    finished 4_average_assistance_time.sql

--- 4_queries/3_assistance_requests_data.sql
+++ 4_queries/3_assistance_requests_data.sql
@@ -1,5 +1,6 @@
-SELECT count(assistance_requests.*) AS total_assistances, students.name AS name
-FROM students
-JOIN assistance_requests ON students.id = student_id
-WHERE name = 'Elliot Dickinson'
-GROUP BY name;
+SELECT teachers.name AS teacher, students.name AS student, assignments.name AS assignment, (assistance_requests.completed_at - assistance_requests.started_at) AS duration 
+FROM assistance_requests
+JOIN assignments ON assignment_id = assignments.id
+JOIN students ON students.id = student_id
+JOIN teachers ON teachers.id = teacher_id
+ORDER BY duration ASC;
